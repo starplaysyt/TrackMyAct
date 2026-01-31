629c454c5fad70c4753da201ceb400b7a88fdd45
vil <vitalik.levchuk.008@gmail.com>
2026-01-31 21:50:34 +0300

    App.kt:
    - Main function of application in commonMain module

---
+++ TrackMyAct.Mobile/composeApp/src/commonMain/kotlin/org/js/tma/App.kt
@@ -0,0 +1,11 @@
+package org.js.tma
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.js.tma.wrapper.AppPreviewWrapper
+
+@Composable
+@Preview
+fun App() {
+    AppPreviewWrapper()
+}
