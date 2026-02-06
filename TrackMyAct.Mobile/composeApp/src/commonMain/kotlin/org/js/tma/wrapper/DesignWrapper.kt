9f4c5ddb1f0ad18066f0438ff2bd65793ee7ebb9
vil <vitalik.levchuk.008@gmail.com>
2026-02-06 15:45:48 +0300

    Editing DesignWrapper.kt:
    - Adding a lot of enumerations to AppScreen enum
    - Creating AppBarPage enum class to enumerate bottom bar pages
    - Editing AppBottomBar function, optimization and redesign

--- TrackMyAct.Mobile/composeApp/src/commonMain/kotlin/org/js/tma/wrapper/DesignWrapper.kt
+++ TrackMyAct.Mobile/composeApp/src/commonMain/kotlin/org/js/tma/wrapper/DesignWrapper.kt
@@ -3,49 +3,92 @@ package org.js.tma.wrapper
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.lifecycle.viewmodel.compose.viewModel
+import org.jetbrains.compose.resources.painterResource
 import org.js.tma.AppTheme
+import org.js.tma.LocalUserData.currentBottomBarSelected
+import org.js.tma.LocalUserData.currentScreen
+import org.js.tma.data.stateValue
 import org.js.tma.getBackground
 import org.js.tma.ui.*
+import org.js.tma.viewmodel.RegistrationViewModel
+import trackmyact.composeapp.generated.resources.*
 
-enum class AppScreen (val needBottomBar: Boolean = true) {
-    Login(false),
-    RegStep1(false),
-    RegStep2(false),
-    RegStep3(false),
-    Categories,
-    CreateEvent,
+enum class AppScreen(val needBottomBar: Boolean = true) {
+    LOGIN(false),
+    REG_STEP_1(false),
+    REG_STEP_2(false),
+    REG_STEP_3(false),
+    ALL_CATEGORIES,
+    MY_CATEGORIES,
+    ALL_ORGANIZERS,
+    MY_ORGANIZERS,
+    ORGANIZER,
+    ACCOUNT,
+    CREATE_EVENT,
+    ALL_EVENTS,
+    MY_EVENTS,
+    EVENT,
+    INVITE_BY_ORGANIZER,
+    INVITE_BY_STUDENT,
+    MY_INVITES,
+    VISIT_CHECK,
+    VISIT_REQUESTS,
     Test
+}
+
+enum class AppBarPage(val title: String, val painter: @Composable () -> Painter) {
+    NOTHING("Ничего", {painterResource(Res.drawable.n_a)}),
+    FOOTBALL("Футбол", {painterResource(Res.drawable.football_page)}),
+    MAIN("Главная", {painterResource(Res.drawable.main_page)}),
+    CALENDAR("Календарь", {painterResource(Res.drawable.calendar_page)}),
+    ACCOUNT("Аккаунт", {painterResource(Res.drawable.account_page)}),
 }
 
 @Composable
 fun AppPreviewWrapper(
-    screenToShow: AppScreen = AppScreen.RegStep3,
-    isDarkTheme: Boolean = true
+    isDarkTheme: Boolean = false,
+    registrationViewModel: RegistrationViewModel = viewModel{ RegistrationViewModel() }
 ) {
     AppTheme(darkTheme = isDarkTheme) {
-        Scaffold (
+        Scaffold(
             bottomBar = {
-                if (screenToShow.needBottomBar) {
-                    AppBottomBar()
+                if (currentScreen.stateValue().needBottomBar) {
+                    AppBottomBar(darkTheme = isDarkTheme)
                 }
             },
         ) { innerPadding ->
-            Box(modifier = getBackground()
-                .padding(innerPadding)
-                .fillMaxSize()
+            Box(
+                modifier = getBackground()
+                    .padding(innerPadding)
+                    .fillMaxSize()
             ) {
-                when (screenToShow) {
-                    AppScreen.Login -> LoginScreen(isDarkTheme)
-                    AppScreen.RegStep1 -> RegistrationStep1Screen(isDarkTheme)
-                    AppScreen.RegStep2 -> RegistrationStep2Screen(isDarkTheme)
-                    AppScreen.RegStep3 -> RegistrationStep3Screen(isDarkTheme)
-                    AppScreen.Categories -> CategoriesScreen()
-                    AppScreen.CreateEvent -> CreateEventScreen()
+                when (currentScreen.stateValue()) {
+                    AppScreen.LOGIN -> LoginScreen(isDarkTheme)
+                    AppScreen.REG_STEP_1 -> RegistrationStep1Screen(isDarkTheme, registrationViewModel)
+                    AppScreen.REG_STEP_2 -> RegistrationStep2Screen(isDarkTheme, registrationViewModel)
+                    AppScreen.REG_STEP_3 -> RegistrationStep3Screen(isDarkTheme, registrationViewModel)
+                    AppScreen.ALL_CATEGORIES -> CategoriesScreen()
+                    AppScreen.CREATE_EVENT -> CreateEventScreen()
                     AppScreen.Test -> TestScreen()
+                    AppScreen.MY_CATEGORIES -> TODO()
+                    AppScreen.ALL_ORGANIZERS -> TODO()
+                    AppScreen.MY_ORGANIZERS -> TODO()
+                    AppScreen.ORGANIZER -> TODO()
+                    AppScreen.ACCOUNT -> TODO()
+                    AppScreen.ALL_EVENTS -> TODO()
+                    AppScreen.MY_EVENTS -> TODO()
+                    AppScreen.EVENT -> TODO()
+                    AppScreen.INVITE_BY_ORGANIZER -> TODO()
+                    AppScreen.INVITE_BY_STUDENT -> TODO()
+                    AppScreen.MY_INVITES -> TODO()
+                    AppScreen.VISIT_CHECK -> TODO()
+                    AppScreen.VISIT_REQUESTS -> TODO()
                 }
             }
         }
@@ -53,39 +96,36 @@ fun AppPreviewWrapper(
 }
 
 @Composable
-fun AppBottomBar() {
+fun AppBottomBar(
+    darkTheme: Boolean
+) {
     NavigationBar(
-        containerColor = MaterialTheme.colorScheme.background
+        containerColor = if (darkTheme) Color(red = 0, green = 0, blue = 0, alpha = 64) else Color(red = 255, green = 255, blue = 255, alpha = 64),
     ) {
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.Menu, contentDescription = null) },
-            label = { Text("Меню") },
-            selected = false,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.SportsSoccer, contentDescription = null) },
-            label = { Text("События") },
-            selected = false,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.Home, contentDescription = null) },
-            label = { Text("Главная") },
-            selected = true,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.CalendarToday, contentDescription = null) },
-            label = { Text("Календарь") },
-            selected = false,
-            onClick = {}
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.Person, contentDescription = null) },
-            label = { Text("Аккаунт") },
-            selected = false,
-            onClick = {}
-        )
+        for (item in AppBarPage.entries) {
+            NavigationBarItem(
+                icon = {
+                    Icon(painter = item.painter(), contentDescription = null)
+                },
+                label = {
+                    Text(
+                        text = item.title,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.labelSmall,
+                        color = if (currentBottomBarSelected.stateValue() == item) colorScheme.outline else colorScheme.outlineVariant
+                    )
+                },
+                selected = currentBottomBarSelected.stateValue() == item,
+                onClick = {
+                    currentBottomBarSelected.value = item
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.Transparent,
+
+                    selectedIconColor = colorScheme.outline,
+                    unselectedIconColor = colorScheme.outlineVariant,
+                )
+            )
+        }
     }
 }
